c66b908719be6e91c84c384c99571f9e3d3551ea
pushspender <vermapushpender2003@gmail.com>
2025-12-23 15:02:16 +0530

    refactor: update Student struct definition and remove unused code

--- StudentResultSystem.c
+++ StudentResultSystem.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+struct Student
+{
+    char name[50];
+    int Id;
+    char batch[50];
+    int marks[];
+    
+
+
+}typedef Student;
+
+
 int main(){
      printf("Hello ");
     return 0;
