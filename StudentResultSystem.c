a6b1ddf04ce6b08da8b24c1903b0532214af81f5
Dev Dave <davedev7867@gmail.com>
2025-12-24 22:40:34 +0530

    All Bugs fixed

--- StudentResultSystem.c
+++ StudentResultSystem.c
@@ -29,6 +29,48 @@ void removeNewline(char *str)
     }
 }
 
+void calculateResult(Student *std,int n){
+    int i,j;
+    float sum;
+
+    //total marks
+    for( i=0;i<n;i++){
+    sum=0;
+     for(int j=0;j<5;j++){
+     sum=sum+std[i].marks[j];
+     }
+     std[i].totalMarks=sum;
+     std[i].percentage=sum/5;
+}
+    
+ 
+ for(int i=0;i<n;i++){
+    if(std[i].percentage>85 ){
+    std[i].grade='A';
+
+    }
+    else if(std[i].percentage>70 ){
+        std[i].grade='B';
+
+    }
+   else if(std[i].percentage>55 ){
+        std[i].grade='C';
+
+    }
+    else if(std[i].percentage>40){
+        std[i].grade='D';
+
+    }
+    else if(std[i].percentage>33){
+        std[i].grade='E';
+
+    }
+    else if(std[i].percentage>0){
+        std[i].grade='F';
+
+    }
+}
+}
 void defaultData(Student *stu){
 
 // Student 0
@@ -84,18 +126,15 @@ stu[4].id = 202504;
 strcpy(stu[4].name, "Komal");
 strcpy(stu[4].batch, "MCA");
 stu[4].grade = 'B';
-stu[4].marks[0] = 80;
-stu[4].marks[1] = 82;
-stu[4].marks[2] = 95;
-stu[4].marks[3] = 86;
-stu[4].marks[4] = 65;
+stu[4].marks[0] = 30;
+stu[4].marks[1] = 52;
+stu[4].marks[2] = 45;
+stu[4].marks[3] = 66;
+stu[4].marks[4] = 35;
 
 dataSize = 5;
-
+calculateResult(stu, dataSize);
 }
-
-    
-
 
 // taking input as from user    
 
@@ -104,29 +143,27 @@ void takingInput(Student *stu,int n)
     for (int i=0; i < n; i++)
     {
         printf("Enter Id of Student %d: ", i+1);
-        scanf("%d", &stu[i].id);
+        scanf("%d", &stu[dataSize].id);
         getchar();   
 
-        printf("Enter Name of  Student %d: ", i+1);
-        fgets(stu[i].name, 50, stdin);
-        removeNewline(stu[i].name);
+        printf("Enter Name of Student %d: ", i+1);
+        fgets(stu[dataSize].name, 50, stdin);
+        removeNewline(stu[dataSize].name);
 
-        printf("Enter Batch of  Student %d: ", i+1);
-        fgets(stu[i].batch, 50, stdin);
-        removeNewline(stu[i].batch);
+        printf("Enter Batch of Student %d: ", i+1);
+        fgets(stu[dataSize].batch, 50, stdin);
+        removeNewline(stu[dataSize].batch);
 
 
         // the loop is used for inner array marks till (0-->4) 5 ele
         for (int j = 0; j < 5; j++)
         {
-            printf("\nEnter marks for %d Subject for Student : ",j+1);
-            scanf("%f",&stu[i].marks[j]);
+            char subs[5][15] = {"OS", "DSA", "CN", "Maths", "Programming"};
+            printf("\nEnter marks for %s (Student %d): ",subs[j],i+1);
+            scanf("%f",&stu[dataSize].marks[j]);
         }
-        
+        dataSize++;
     }
-    dataSize += n;
-    
-    
 }
 
 
@@ -136,58 +173,12 @@ void printAllData(Student *std, int n){
     printf("%-6s %-20.20s %-8.8s %-6s %-6s %-6s %-6s %-7s %-6s %-10s %-10s\n", "Id","Name","Batch","Grade","OS","DSA","CN","Maths","Progr.","totalMarks","percentage");
     for (int i = 0; i < n; i++)
     {
-        printf("---------------------------------------------------------------------------------\n");
+        printf("----------------------------------------------------------------------------------------------------------------\n");
         printf("%-6d %-20.20s %-8.8s %-6c %-6.2f %-6.2f %-6.2f %-7.2f %-6.2f %-6.2f\n", std[i].id,std[i].name,std[i].batch ,std[i].grade,std[i].marks[0],std[i].marks[1],std[i].marks[2],std[i].marks[3],std[i].marks[4], std[i].totalMarks);
     } 
 }
 
-void calculateResult(Student *std,int n){
-    int i,j;
-    float sum;
-    
-     
-    
 
-    //total marks
-    for( i=0;i<n;i++){
-    sum=0;
-     for(int j=0;j<5;j++){
-     sum=sum+std[i].marks[j];
-     }
-     printf("%d \n", sum);
-     std[i].totalMarks=sum;
-     std[i].percentage=sum/5;
-}
-    
- 
- for(int i=0;i<n;i++){
-    if(std[i].percentage>85 ){
-    std[i].grade='A';
-
-    }
-    else if(std[i].percentage>70 ){
-        std[i].grade='B';
-
-    }
-   else if(std[i].percentage>55 ){
-        std[i].grade='C';
-
-    }
-    else if(std[i].percentage>40){
-        std[i].grade='D';
-
-    }
-    else if(std[i].percentage>33){
-        std[i].grade='E';
-
-    }
-    else if(std[i].percentage>0){
-        std[i].grade='F';
-
-    }
-    }
-
-}
 
 void menu(Student *stu){
 
@@ -196,7 +187,7 @@ void menu(Student *stu){
     while (option)
     {
         int n;
-
+            printf("");
             printf("---------Choose Option To Perform a Task------------------\n");
             printf("To Add Data choose 1\n");
             printf("To Display  Data choose 2\n");
@@ -209,7 +200,7 @@ void menu(Student *stu){
                 scanf("%d",&n);
                 // this function will take input form user
                 takingInput(stu,n);
-                calculateResult(stu,n);
+                calculateResult(stu,dataSize);
             }else if(option == 2){
                 printAllData(stu,dataSize);    
             }else
@@ -218,8 +209,6 @@ void menu(Student *stu){
             }
     }
 }
-
-
 
 int main(){
     struct Student stu[N];
