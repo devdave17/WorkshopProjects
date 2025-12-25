eddfce55a4bd6f8ae541228629770499f45aecde
Radhika Verma <radhika.verma08007@gmail.com>
2025-12-25 11:39:36 +0530

    Add structure

--- ContactManagementSystem.c
+++ ContactManagementSystem.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#include<string.h>
+#define N 100
+
+typedef struct Contact{
+    int id;
+    char name[50];
+    int phone;
+    char email[50];
+    char location[50]; 
+}Contact;
 
 int main(){
-
-
-    return 0;
+    struct Contact cont[N];
 }
