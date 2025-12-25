d7b8e2a89028448cb9bc156ba75fda113e174f04
Radhika Verma <radhika.verma08007@gmail.com>
2025-12-25 11:42:05 +0530

    ADD

--- ContactManagementSystem.c
+++ ContactManagementSystem.c
@@ -3,13 +3,13 @@
 #define N 100
 
 typedef struct Contact{
-    int id;
     char name[50];
     int phone;
-    char email[50];
-    char location[50]; 
 }Contact;
 
+
 int main(){
+
     struct Contact cont[N];
+
 }
